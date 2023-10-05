f843e1ce83023d6bf588a16f9edf7668b624b297
Helena Rentschler <helrentschler@gmail.com>
2023-10-05 18:58:00 -0300

    crud

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,2 +1,26 @@
 const mongoose = require("mongoose");
 
+const userSchema = new mongoose.Schema({
+
+    firstName: {
+        type: String,
+        required: true
+    },
+    lastName: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 7
+    },
+});
+
+const userModel = mongoose.model("User", userSchema);
+
+module.exports = userModel;
--- src/modules/express.js
+++ src/modules/express.js
@@ -1,30 +1,105 @@
+//Deinindo Endpoints - crud
+
 const express = require("express");
+const UserModel = require("../models/user.model")
 
 const app = express();
 
-//setando callback para requisiçao get em 
-app.get("/", (req, res) => {
-    res.status(200).send("<h1> Home </h1>");
+app.use(express.json()); //vamos sempre usar json no corpo das requisiçoes, tanto post qaunto get
+
+//create user
+//setando resposta da requisiçao post user
+//assincrona pois UserModel.create retorna promise
+//para teste, informaçoes do usuario sao adicionadas no corpo da requisiçao post pelo postman
+app.post("/users", async (req,res) => {
+    
+    try {
+        
+        const user = await UserModel.create(req.body);
+
+        //status 201: criado e obtido com sucesso
+        res.status(201).json(user);
+        
+    } catch (error) {
+        
+        res.status(500).send(error.message);
+    }
 });
 
-app.get("/users", (req, res) => {
+//read users
+//setando resposta da requisiçao get users
+app.get("/users", async (req, res) => {
 
-    const users = [
-        {
-            nome: "John Doe",
-            email: "john@gmail.com"
-        },
-        {
-            nome: "Ana Doe",
-            email: "ana@gmail.com"
-        },
-        {
-            nome: "Paul Richards",
-            email: "paul@gmail.com"
-        }
-    ]
+    try {
+        //sem parametros de filtro, traz todos users
+        const users = await UserModel.find({});
 
-    res.status(200).json(users);
+        //status 200: obtidos com sucesso
+        res.status(200).json(users);
+        
+    } catch (error) {
+        
+        res.status(500).send(error.message);
+    }
+});
+
+//read user/id
+//setando resposta da requisiçao get user/id
+app.get("/users/:id", async (req, res) => {
+
+    try {
+        const id = req.params.id;
+        //retorna o user com o id correspondente
+        const user = await UserModel.findById(id);
+
+        //status 200: json obtido com sucesso
+        res.status(200).json(user);
+        
+    } catch (error) {
+        
+        res.status(500).send(error.message);
+    }
+
+});
+
+//update user/id
+//setando resposta da requisiçao patch user/id
+app.patch("/users/:id", async (req, res) => {
+
+    try {
+        const id = req.params.id;
+
+        //recebe json do user com seu campo enviado por body atualizado (new)
+        const user = await UserModel.findByIdAndUpdate(id, req.body, { new : true });
+
+        //status 200: atualizado e json obtido com sucesso
+        res.status(200).json(user);
+        
+    } catch (error) {
+        
+        res.status(500).send(error.message);
+    }
+
+});
+
+//delete user/id
+//setando resposta da requisiçao delete user/id
+app.delete("/users/:id", async (req, res) => {
+
+    try {
+        const id = req.params.id;
+
+        //remove e retorna usuario removido
+        const user = await UserModel.findByIdAndRemove(id);
+
+        //status 200: deletado e json obtido com sucesso
+        res.status(200).json(user);
+        
+    } catch (error) {
+        
+        res.status(500).send(error.message);
+    }
+
 });
 
 const port = 8080;
